b3c8d92610e417c5dcf9616f6401e780e6496a75
Gauri <gaurihate03@gmail.com>
2026-01-12 22:35:50 +0530

    final project resolve error in deployment process

--- server.js
+++ server.js
@@ -1,46 +1,45 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
-import cors from 'cors'
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 
-// configure env (MUST be first) loads values from the .env
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
-// rest object
 const app = express();
 
-// database config 
-// Calls your MongoDB connection function
-// When server starts → database gets connected
-// Usually uses Mongoose inside db.js
+// DB
 connectDB();
 
-//  Middleware = code that runs before routes
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-//routes Route Grouping
-app.use('/api/v1/auth', authRoute)  // api for authroute file in routes----->authConyroller(overall logic of route) ROUTE GROUPPING
-app.use('/api/v1/category', categoryRoute)
+// API routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoutes);
 
-// rest api
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome</h1>");
+// Serve frontend
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+// ✅ Express 5 compatible React fallback
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "client/dist/index.html"));
 });
 
-// port
 const PORT = process.env.PORT || 8080;
 
-// run server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
-
